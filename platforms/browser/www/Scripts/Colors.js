9e4526cd0cc01762c5839e58585ed33fc8cff6c2
Komodoro <j_diniz1@msn.com>
2017-02-18 20:15:05 +0000

    Start Source
    Added project Source

---
+++ platforms/browser/www/Scripts/Colors.js
@@ -0,0 +1,16 @@
+//var tram15 = new Image();
+//    this.tram15.src = '15Tram.png';
+var colors = 
+{
+	1:'black',
+	2:'green',
+	3:'blue',
+	4:'yellow',
+	5:'violet',
+
+	'black': 1,
+	'green': 2,
+	'blue': 3,
+	'yellow': 4,
+	'violet' : 5
+}
